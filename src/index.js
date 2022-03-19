446de58bffee4f53bbbb3be38b8a4f9e6146122a
jrenzzo <renzo-hg03@hotmail.com>
2022-03-19 15:44:05 -0400

    Added project and task pop ups

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
 // (at the end copy the other css file) include for using css as part of webpack:
 //import './style.css';
+function openProjectPopUp(){
+    const projectPopup = document.querySelector('.proj-popup')
+    projectPopup.classList.add('active')
+
+}
