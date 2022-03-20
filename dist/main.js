3ffe14872754aeb4b5b9625007d9d4c657222e92
jrenzzo <renzo-hg03@hotmail.com>
2022-03-20 14:00:50 -0400

    Added option to close popup

---
+++ dist/main.js
@@ -0,0 +1,1 @@
+(()=>{const e=document.querySelector(".edit-popup-container");document.querySelector(".add-project").addEventListener("click",(function(){e.classList.add("active"),console.log("open")})),document.querySelector(".sub-cancel").addEventListener("click",(function(){e.classList.remove("active"),console.log("close")}))})();
--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
 // (at the end copy the other css file) include for using css as part of webpack:
 //import './style.css';
-function openProjectPopUp(){
-    const projectPopup = document.querySelector('.proj-popup')
-    projectPopup.classList.add('active')
+const projectPopup = document.querySelector('.edit-popup-container')
 
+function openProjectPopUp(){
+    projectPopup.classList.add('active')
+    console.log('open')
 }
+
+function closeProjectPopup(){
+    projectPopup.classList.remove('active')
+    console.log('close')
+}
+
+const addButton = document.querySelector('.add-project')
+addButton.addEventListener('click', openProjectPopUp)
+
+const subCancelProj = document.querySelector('.sub-cancel')
+subCancelProj.addEventListener('click', closeProjectPopup)
+
+
