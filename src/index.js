493473640cffcb34d7fcdb43ca8dfa27ba945014
jrenzzo <renzo-hg03@hotmail.com>
2022-03-20 14:59:53 -0400

    Added interface file

--- src/index.js
+++ src/index.js
@@ -1,21 +1,8 @@
 // (at the end copy the other css file) include for using css as part of webpack:
 //import './style.css';
-const projectPopup = document.querySelector('.edit-popup-container')
 
-function openProjectPopUp(){
-    projectPopup.classList.add('active')
-    console.log('open')
-}
+import activateProjectOptions from './interface'
 
-function closeProjectPopup(){
-    projectPopup.classList.remove('active')
-    console.log('close')
-}
-
-const addButton = document.querySelector('.add-project')
-addButton.addEventListener('click', openProjectPopUp)
-
-const subCancelProj = document.querySelector('.sub-cancel')
-subCancelProj.addEventListener('click', closeProjectPopup)
+activateProjectOptions()
 
 
---
+++ src/interface.js
@@ -0,0 +1,46 @@
+const projectPopup = document.querySelector('.proj-popup')
+const taskPopup = document.querySelector('.task-popup')
+
+function activateProjectOptions(){
+    const addProjectButton = document.querySelector('.add-project')
+    const subBtnAddProj = document.querySelector('.sub-add-proj')
+    const subBtnCancelProj = document.querySelector('.sub-cancel-proj')
+    
+    addProjectButton.addEventListener('click', openProjectPopup)
+    subBtnAddProj.addEventListener('click', addNewProject)
+    subBtnCancelProj.addEventListener('click', closeProjectPopup)
+}
+
+function openProjectPopup(){
+    projectPopup.classList.add('active')
+}
+
+function closeProjectPopup(){
+    projectPopup.classList.remove('active')
+}
+
+function addNewProject(){
+    console.log('a')
+}
+
+function activateTasksOptions(){
+    const addTaskButton = document.querySelector('.add-task')
+    const subBtnAddTask = document.querySelector('.sub-add-task')
+    const subBtnCancelTask = document.querySelector('.sub-cancel-task')
+
+    addTaskButton.addEventListener('click', openTaskPopup)
+    subBtnAddTask.addEventListener('click', () =>{
+        console.log('a')
+    })
+    subBtnCancelTask.addEventListener('click', closeTaskPopup)
+}
+
+function openTaskPopup(){
+    taskPopup.classList.add('active')
+}
+
+function closeTaskPopup(){
+    taskPopup.classList.remove('active')
+}
+
+export default activateProjectOptions
